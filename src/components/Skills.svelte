4cb5df0f867b10d7ff62bfafe6a70354a20099de
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-02-04 12:14:42 +0300

    feat: add more skills

--- src/components/Skills.svelte
+++ src/components/Skills.svelte
@@ -2,10 +2,13 @@
   import Subtitle from "./Subtitle.svelte";
 
   const skills = [
-    { name: "React.js", bgColor: "#149eca", color: "#ffffff" },
-    { name: "Next.js", bgColor: "#000000", color: "#ffffff" },
-    { name: "TypeScript", bgColor: "#3178c6", color: "#ffffff" },
-    { name: "Go", bgColor: "#79d4fd", color: "#000000" },
+    { name: "React.js", url: "https://reactjs.org", bgColor: "#149eca", color: "#ffffff" },
+    { name: "Next.js", url: "https://nextjs.org", bgColor: "#000000", color: "#ffffff" },
+    { name: "TypeScript", url: "https://typescriptlang.org", bgColor: "#3178c6", color: "#ffffff" },
+    { name: "Tailwind", url: "https://tailwindcss.com", bgColor: "#38bdf8", color: "#ffffff" },
+    { name: "Go", url: "https://go.dev", bgColor: "#79d4fd", color: "#000000" },
+    { name: "Gorm", url: "https://gorm.io", bgColor: "#3f424a", color: "#ffffff" },
+    { name: "fiber", url: "https://gofiber.io", bgColor: "#00acd7", color: "#ffffff" },
   ];
 </script>
 
@@ -13,9 +16,13 @@
   <Subtitle class="mb-2">Skills</Subtitle>
   <div class="flex gap-2 flex-wrap">
     {#each skills as skill, i (i)}
-      <span
+      <a
+        href={skill.url}
+        target="_blank"
+        rel="noopener noreferrer"
         style={`background-color: ${skill.bgColor}; color: ${skill.color}`}
-        class="py-1 px-2 rounded-md font-medium font-inter">{skill.name}</span
+        class="py-1 px-2 rounded-md font-medium font-inter transition-all hover:no-underline hover:opacity-70"
+        >{skill.name}</a
       >
     {/each}
   </div>
